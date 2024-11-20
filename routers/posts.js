774930aed2b4fa77875653b5eca151d1f30bcdfb
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-20 15:47:29 +0100

    add bonus

--- routers/posts.js
+++ routers/posts.js
@@ -43,8 +43,39 @@ res.json({
 
 // Show
 router.get ("/:id", (req,res)=>{
+    const posts =[
+        {   
+            titolo: "Ciambellone",
+            contenuto:"",
+            img:"./public/img/ciambellone.jpeg",
+            tags : ["ciambellone", "dolce"]
+        },
+        {   
+            titolo: "Cracker alla barbabietola",
+            contenuto:"",
+            img:"./public/img/cracker_barbabietola.jpeg",
+            tags : ["snack","salato"]
+        },
+        {   
+            titolo: "Pane fritto",
+            contenuto:"",
+            img:"./public/img/cracker_barbabietola.jpeg",
+            tags : ["pane", "cina"]
+        },
+        {   
+            titolo: "Pasta alla barbabietola",
+            contenuto:"",
+            img:"./public/img/pasta_barbabietola.jpeg",
+            tags : ["pasta", "barbabietola", "rosso"]
+        },
+        {   
+            titolo: "Torta paesana",
+            contenuto:"",
+            img:"./public/img/torta_paesana.jpeg",
+            tags : ["tradizione", "torta", "dolce"]
+        }]
     const {id} = req.params;
-    res.json(`mostra il post con id : ${id}`);
+    res.json(posts[id]);
 
 });
 
