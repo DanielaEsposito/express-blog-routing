1d7986fcd97410ae35015bac18fcf3133f892a6b
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-20 13:45:06 +0100

    add routers

--- app.js
+++ app.js
@@ -1,52 +1,20 @@
 
 const express = require('express');
-const { url } = require('inspector');
 const app = express();
-const port = 3000
-app.use(express.static('public'))
+const port = 3000;
+
+app.use(express.static('public'));
+
+const postsRouter = require("./routers/posts");
+
+app.use("./posts", postsRouter);
 
 app.get('/',(req, res)=> {
     res.send ("server del mio blog")
 })
 
-app.get('/bacheca',(req, res)=> {
-    const posts =[
-{   
-    titolo: "Ciambellone",
-    contenuto:"",
-    img:"./public/img/ciambellone.jpeg",
-    tags : ["ciambellone", "dolce"]
-},
-{   
-    titolo: "Cracker alla barbabietola",
-    contenuto:"",
-    img:"./public/img/cracker_barbabietola.jpeg",
-    tags : ["snack","salato"]
-},
-{   
-    titolo: "Pane fritto",
-    contenuto:"",
-    img:"./public/img/cracker_barbabietola.jpeg",
-    tags : ["pane", "cina"]
-},
-{   
-    titolo: "Pasta alla barbabietola",
-    contenuto:"",
-    img:"./public/img/pasta_barbabietola.jpeg",
-    tags : ["pasta", "barbabietola", "rosso"]
-},
-{   
-    titolo: "Torta paesana",
-    contenuto:"",
-    img:"./public/img/torta_paesana.jpeg",
-    tags : ["tradizione", "torta", "dolce"]
-}]
-res.json({
-    number : posts.length,
-    posts :posts
-})
-})
 
+;
 app.listen(port ,()=>{
     console.log("il server è online");
     
---
+++ routers/posts.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/bacheca',(req, res)=> {
+    const posts =[
+{   
+    titolo: "Ciambellone",
+    contenuto:"",
+    img:"./public/img/ciambellone.jpeg",
+    tags : ["ciambellone", "dolce"]
+},
+{   
+    titolo: "Cracker alla barbabietola",
+    contenuto:"",
+    img:"./public/img/cracker_barbabietola.jpeg",
+    tags : ["snack","salato"]
+},
+{   
+    titolo: "Pane fritto",
+    contenuto:"",
+    img:"./public/img/cracker_barbabietola.jpeg",
+    tags : ["pane", "cina"]
+},
+{   
+    titolo: "Pasta alla barbabietola",
+    contenuto:"",
+    img:"./public/img/pasta_barbabietola.jpeg",
+    tags : ["pasta", "barbabietola", "rosso"]
+},
+{   
+    titolo: "Torta paesana",
+    contenuto:"",
+    img:"./public/img/torta_paesana.jpeg",
+    tags : ["tradizione", "torta", "dolce"]
+}]
+res.json({
+    number : posts.length,
+    posts :posts
+})
+})
+module.exports()
